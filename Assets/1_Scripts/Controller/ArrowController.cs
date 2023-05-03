a625dd3f3863b096baf6fb2f28e5bfa10e3846c0
BJH7536 <a01091363615@gmail.com>
2023-05-04 00:19:58 +0900

    [FEAT] ArrowController에 플레이어를 따라가는 기능 추가 (-정훈)
    [FEAT]
    ArrowController에 플레이어를 따라가는 기능 추가
    (-정훈)

--- Assets/1_Scripts/Controller/ArrowController.cs
+++ Assets/1_Scripts/Controller/ArrowController.cs
@@ -8,6 +8,12 @@ public class ArrowController : MonoBehaviour
     public string Symbol;       // Symbol을 Inspector에서 간편하게 설정할 수 있도록,
     TextMeshPro tmp;            // 화살의 Symbol이 표시될 TextMeshPro
 
+    Vector2 dirVec;
+    public GameObject player;
+
+    float curShotDelay = 0f;
+    float maxShotDelay = 0f;
+
     private void Start()
     {
         tmp = GetComponentInChildren<TextMeshPro>();
@@ -19,6 +25,39 @@ public class ArrowController : MonoBehaviour
             Symbol = Random.Range(0, 10).ToString();
             tmp.text = Symbol;
         }
-
+        //
+        maxShotDelay = Random.Range(1f, 2f);
+        //
     }
+
+    void Update()
+    {
+        dirVec = player.transform.position - transform.position;
+
+        Shot();
+        Reload();
+    }
+
+    void Shot()
+    {
+        if (curShotDelay < maxShotDelay) return;
+      
+        GameObject testBullet = Managers.Resource.Instantiate("TestBullet");
+        testBullet.transform.position = transform.position;
+        Rigidbody2D testBulletRigid = testBullet.GetComponent<Rigidbody2D>();
+        testBulletRigid.AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);
+
+        Debug.Log(" + bullet shot!");
+
+        Destroy(testBullet, 2f);
+
+        curShotDelay = 0;
+    }
+
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime;
+    }
+
+
 }
