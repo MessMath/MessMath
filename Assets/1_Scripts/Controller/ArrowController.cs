3a7ce8333febcb5dfe98a6343942996a263aec93
BJH7536 <a01091363615@gmail.com>
2023-05-08 00:11:34 +0900

    [FEAT] Assets\1_Scripts\Controller\ArrowController.cs (-정훈)
    [FEAT]
    Assets\1_Scripts\Controller\ArrowController.cs
    Assets\Resources\Prefabs\TestBullet.prefab
    플레이어를 바라보고 발사되도록 기능 추가 + 50%의 확률로 사칙연산 연산자로 정해지는 기능 추가
    (-정훈)

--- Assets/1_Scripts/Controller/ArrowController.cs
+++ Assets/1_Scripts/Controller/ArrowController.cs
@@ -7,12 +7,9 @@ public class ArrowController : MonoBehaviour
 {
     public string Symbol;       // Symbol을 Inspector에서 간편하게 설정할 수 있도록,
     TextMeshPro tmp;            // 화살의 Symbol이 표시될 TextMeshPro
-
+    string[] Operator = { "+", "-", "×", "÷" };
     Vector2 dirVec;
     public GameObject player;
-
-    float curShotDelay = 0f;
-    float maxShotDelay = 0f;
 
     private void Start()
     {
@@ -22,42 +19,36 @@ public class ArrowController : MonoBehaviour
         if (Symbol.Length > 1)  // Symbol의 길이가 1이 넘어간다면 임의의 1의자리 자연수로 대체.
         {
             Debug.Log("Symbole Length should be 1!!");
-            Symbol = Random.Range(0, 10).ToString();
+
+            if (Random.Range(0, 2) == 1)
+                Symbol = Random.Range(0, 10).ToString();            // 50%의 확률로 Symbol이 0~9의 숫자고
+            else
+                Symbol = Operator[Random.Range(0, 4)].ToString();   // 50%의 확률로 Symbol이 사칙연산 중 하나의 기호에 해당한다.
+
             tmp.text = Symbol;
         }
-        //
-        maxShotDelay = Random.Range(1f, 2f);
-        //
-    }
 
-    void Update()
-    {
+        if (player == null)     
+            player = GameObject.FindGameObjectWithTag("Player"); // Player를 찾지못하는 오류를 고치기 위한 코드
+
+        LookAt(player);     // Player를 바라보고 날라가게끔
+
         dirVec = player.transform.position - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);
 
-        Shot();
-        Reload();
     }
 
-    void Shot()
+    void LookAt(GameObject target)
     {
-        if (curShotDelay < maxShotDelay) return;
-      
-        GameObject testBullet = Managers.Resource.Instantiate("TestBullet");
-        testBullet.transform.position = transform.position;
-        Rigidbody2D testBulletRigid = testBullet.GetComponent<Rigidbody2D>();
-        testBulletRigid.AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);
+        if(target != null)
+        {
+            Vector2 direction = new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y);
 
-        Debug.Log(" + bullet shot!");
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion angleAxis = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+            Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, 1);
 
-        Destroy(testBullet, 2f);
-
-        curShotDelay = 0;
+            transform.rotation = rotation;
+        }
     }
-
-    void Reload()
-    {
-        curShotDelay += Time.deltaTime;
-    }
-
-
 }
