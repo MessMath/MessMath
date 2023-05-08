ce9d0d104afc279053b30222939e8a3e13653577
truemanburbank <juxtap.se@gmail.com>
2023-05-08 22:10:34 +0900

    [FIX] PlayerController 오타 수정
    [FIX]MessMath\Assets\0_Scenes\DevScene\UIScene.unity 오타 수정
    [FIX] Player speed 수정

--- Assets/1_Scripts/Controller/PlayerControllerUI.cs
+++ Assets/1_Scripts/Controller/PlayerControllerUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerUI : MonoBehaviour
 {
     public float speed = 3f;
     private JoyStickController joystick;
