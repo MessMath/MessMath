a9c5dfb12141909c87faba1a354f0359c993b50b
BJH <114412598+BJH7536@users.noreply.github.com>
2023-05-10 18:14:23 +0900

    [ADD] Assets\1_Scripts\Controller\WitchController.cs (-정훈)
    [ADD]
    Assets\1_Scripts\Controller\WitchController.cs
    Witch 추가. 체력을 갖고, Questioning을 시도하는 객체의 스크립트
    (-정훈)

---
+++ Assets/1_Scripts/Controller/WitchController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WitchController : MonoBehaviour
+{
+    public int Hp = 100;
+    public Slider HpBar;
+    public int QusetionNumber;
+    public TextMeshProUGUI QusetionNumberText;
+
+    private void Start()
+    {
+        HpBar.value = Hp;
+        Questioning();
+    }
+
+    private void FixedUpdate()
+    {
+        HpBar.value = Hp;
+    }
+
+    void Questioning()
+    {
+        QusetionNumber = Random.Range(1, 100);
+        QusetionNumberText.text = $"{QusetionNumber}";
+    }
+}
