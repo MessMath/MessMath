cf4912608ae2d2d3f8ed3c096c0a573df6bcfb04
BJH <114412598+BJH7536@users.noreply.github.com>
2023-05-10 23:43:25 +0900

    [FIX] Assets\1_Scripts\Controller\ArrowController.cs (-정훈)
    [FIX]
    Assets\1_Scripts\Controller\ArrowController.cs
    잔버그 수정
    (-정훈)

--- Assets/1_Scripts/Controller/ArrowController.cs
+++ Assets/1_Scripts/Controller/ArrowController.cs
@@ -1,13 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
-    public string Symbol;       // Symbol�� Inspector���� �����ϰ� ������ �� �ֵ���,
-    TextMeshPro tmp;            // ȭ���� Symbol�� ǥ�õ� TextMeshPro
-    string[] Operator = { "+", "-", "��", "��" };
+    public string Symbol;       // Symbol을 Inspector에서 간편하게 설정할 수 있도록,
+    TextMeshPro tmp;            // 화살의 Symbol이 표시될 TextMeshPro
+    string[] Operator = { "+", "-", "×", "÷" };
     Vector2 dirVec;
     public GameObject player;
 
@@ -16,23 +16,23 @@ public class ArrowController : MonoBehaviour
         tmp = GetComponentInChildren<TextMeshPro>();
         tmp.text = Symbol;
 
-        if (Symbol.Length > 1)  // Symbol�� ���̰� 1�� �Ѿ�ٸ� ������ 1���ڸ� �ڿ����� ��ü.
+        if (Symbol.Length > 1)  // Symbol의 길이가 1이 넘어간다면 임의의 1의자리 자연수로 대체.
         {
             Debug.Log("Symbol Length should be 1!!");
 
             if (Random.Range(0, 2) == 1)
-                Symbol = Random.Range(0, 10).ToString();            // 50%�� Ȯ���� Symbol�� 0~9�� ���ڰ�
+                Symbol = Random.Range(0, 10).ToString();            // 50%의 확률로 Symbol이 0~9의 숫자고
             else
-                Symbol = Operator[Random.Range(0, 4)].ToString();   // 50%�� Ȯ���� Symbol�� ��Ģ���� �� �ϳ��� ��ȣ�� �ش��Ѵ�.
+                Symbol = Operator[Random.Range(0, 4)].ToString();   // 50%의 확률로 Symbol이 사칙연산 중 하나의 기호에 해당한다.
 
             tmp.text = Symbol;
         }
 
         if (player == null)     
-            player = GameObject.FindGameObjectWithTag("Player"); // Player�� ã�����ϴ� ������ ��ġ�� ���� �ڵ�
+            player = GameObject.FindGameObjectWithTag("Player"); // Player를 찾지못하는 오류를 고치기 위한 코드
 
         dirVec = player.transform.position - transform.position;
-        LookAt(player);     // Player�� �ٶ󺸰� ���󰡰Բ�
+        LookAt(player);     // Player를 바라보고 날라가게끔
         GetComponent<Rigidbody2D>().AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);
 
         GetComponentInChildren<RectTransform>().localRotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z * (-1.0f));
