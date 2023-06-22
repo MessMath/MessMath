91cf0ef87d26811924c187cc91c0385c128f120c
BJH <114412598+BJH7536@users.noreply.github.com>
2023-06-22 21:00:34 +0900

    [ADD] Fight1vs1GameScene 추가(-정훈)
    수학자와 1vs1 대련 학습 기능을 개발하기 위한 준비

---
+++ Assets/1_Scripts/Scene/Fight1vs1Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using UnityEngine;
+
+public class Fight1vs1Game : BaseScene
+{
+    protected override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        SceneType = Define.Scene.Fight1vs1GameScene;
+        Managers.UI.ShowSceneUI<UI_Fight1vs1Game>();
+        Debug.Log("Enter Fight1vs1Game");
+        return true;
+    }
+}
