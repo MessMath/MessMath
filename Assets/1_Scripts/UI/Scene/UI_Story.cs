48fb9b75143586bc6b85dec02eec87fbb09981f5
JiYeon <she79560@naver.com>
2023-06-26 18:39:17 +0900

    [ADD] 스토리 다시보기 추가(-지연)

--- Assets/1_Scripts/UI/Scene/UI_Story.cs
+++ Assets/1_Scripts/UI/Scene/UI_Story.cs
@@ -9,19 +9,19 @@ public class UI_Story : UI_Scene
     int maxCount;
     int count = -1;
     List<TalkData> storyTalkData = new List<TalkData>();
-     enum Images
+    GameObject replayPopup;
+    enum Images
     {
         BackGroundImage,
         PlayerImage,
         CharacterImage,
         FadeImage,
     }
-
     enum Buttons
     {
         nxtButton,
+        ReplayButton,
     }
-
     enum Texts
     {
         CharacterNameTMP,
@@ -45,10 +45,13 @@ public class UI_Story : UI_Scene
         jsonReader = new JsonReader();
         storyTalkData = jsonReader.ReadStoryJson(Application.persistentDataPath + "/" + 0 + "_EnterGameStory.json").talkDataList;
         maxCount = storyTalkData.Count;
+        replayPopup = Managers.UI.ShowPopupUI<UI_ReplayStory>().gameObject;
+        replayPopup.SetActive(false);
 
         GetText((int)Texts.CharacterNameTMP).text = "";
         GetText((int)Texts.DialogueTMP).text = "";
         GetButton((int)Buttons.nxtButton).gameObject.BindEvent(OnClickNxtBtn);
+        GetButton((int)Buttons.ReplayButton).gameObject.BindEvent(OnClickReplayBtn);
         return true;
     }
 
@@ -63,7 +66,6 @@ public class UI_Story : UI_Scene
             Managers.Scene.ChangeScene(Define.Scene.StoryGameScene); 
             return;
         }
-        Debug.Log("Cnt: " + count);
         if(storyTalkData[count].sceneEffect!="") GetButton((int)Buttons.nxtButton).interactable = false;
         Managers.SceneEffect.SceneEffect(GetImage((int)Images.FadeImage),GetButton((int)Buttons.nxtButton), storyTalkData[count].sceneEffect);
         Managers.SceneEffect.ChangeBackground(GetImage((int)Images.BackGroundImage), storyTalkData[count].backgroundImg);
@@ -80,8 +82,15 @@ public class UI_Story : UI_Scene
 
         Managers.TextEffect.SetNormalSpeed();
         Managers.TextEffect.Typing(storyTalkData[count].dialogue, GetText((int)Texts.DialogueTMP));
-
+        replayPopup.SetActive(true);
+        replayPopup.GetComponent<UI_ReplayStory>().AddReplayStory(storyTalkData[count].characterName, storyTalkData[count].dialogue);
+        replayPopup.SetActive(false);
         //talkDatas[i].soundEffect
         //talkDatas[i].soundEffectDuration
+    }
+
+    void OnClickReplayBtn()
+    {
+        replayPopup.SetActive(true);
     }
 }
