a1af35d8b2608731e3aea82076145e7501438a45
BJH7536 <a01091363615@gmail.com>
2023-06-27 19:31:07 +0900

    [ADD] ProtectionManager.cs 추가 (-정훈)
    가호 담당 매니저를 추가했다. 기존에 BlessingManager를 대신한다.
    여기 안에 가호 효과를 함수로 구현해서, 외부에서 접근하는 방식으로 호출하도록 하자.

---
+++ Assets/1_Scripts/Manager/ProtectionManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+/// <summary>
+/// 모든 가호를 관리하는 Manager
+/// </summary>
+public class ProtectionManager
+{
+    PlayerControllerCCF player;
+    WitchController witch;
+    Vector3 playerPos;
+
+    public void Setup()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerCCF>();
+        playerPos = player.transform.position;
+        witch = GameObject.FindGameObjectWithTag("Witch").GetComponent<WitchController>();
+    }
+
+    /// <summary>
+    /// 가우스의 가호 : 세레스 공전, 이 다음 공격 데미지를 두배로.
+    /// </summary>
+    public void ProtectionOfGauss()
+    {
+        Setup();
+        float radius = 200f;
+        float angle = 0f;
+        float speed = 2f;
+        int prevWitchHp = witch.Hp;
+
+        // 공격 데미지 2배
+        Managers.Game.Damage *= 2;
+
+        GameObject Ceres = Managers.Resource.Instantiate("protection/Ceres", player.transform);
+        Ceres.transform.position = new Vector3(playerPos.x - radius, playerPos.y, 0);
+
+        // 세레스 돌리기
+        CoroutineHelper.StartCoroutine(RotateCeres(Ceres, player.transform, radius, angle, speed, prevWitchHp));
+    }
+
+    /// <summary>
+    /// 세레스를 계속 돌린다. 마녀가 데미지를 받으면 멈춤.
+    /// </summary>
+    /// <param name="Ceres">세레스 오브젝트</param>
+    /// <param name="center">중심으로 잡을 오브젝트</param>
+    /// <param name="radius">반지름</param>
+    /// <param name="angle">시작각도</param>
+    /// <param name="speed">속도</param>
+    /// <param name="prevWitchHp">마녀의 이전 체력</param>
+    IEnumerator RotateCeres(GameObject Ceres, Transform center, float radius, float angle, float speed, int prevWitchHp)
+    {
+        while(true)
+        {
+            angle += speed * Time.deltaTime;
+            Ceres.transform.position = center.position + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
+
+            if (prevWitchHp != witch.Hp)
+            {
+                Debug.Log("witch hp : " + witch.Hp);
+                Managers.Game.Damage = Managers.Game.Damage / 2;
+                Object.Destroy(Ceres.gameObject);
+                yield break;
+            }
+
+            yield return null;
+        }
+    }
+
+}
