6e3675da0919841ffefb3d826e9ac578c152fe1f
BJH <114412598+BJH7536@users.noreply.github.com>
2023-06-28 16:38:11 +0900

    [UPDATE] 가호의 명칭을 Grace로 정정&고정  + 가우스가호 중복적용 방지(-정훈)
    가호의 정의와 참조를 모두 수정.
    가우스 가호 중복적용 방지

--- Assets/1_Scripts/Manager/GraceManager.cs
+++ Assets/1_Scripts/Manager/GraceManager.cs
@@ -6,24 +6,43 @@ using UnityEngine;
 /// <summary>
 /// 모든 가호를 관리하는 Manager
 /// </summary>
-public class ProtectionManager
+public class GraceManager
 {
     PlayerControllerCCF player;
     WitchController witch;
     Vector3 playerPos;
+
+    public bool gaussOn = false;
 
     public void Setup()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerCCF>();
         playerPos = player.transform.position;
         witch = GameObject.FindGameObjectWithTag("Witch").GetComponent<WitchController>();
+
+    }
+
+    /// <summary>
+    /// 가호 호출은 모두 CallGrace()를 통해서 한다. Managers.Grace.CallGrace("GraceOfGauss")
+    /// </summary>
+    /// <param name="graceName"> 가호의 이름을 입력 예. CallGrace </param>
+    public void CallGrace(string graceName)
+    {
+        switch (graceName)
+        {
+            case "GraceOfGauss":
+                GraceOfGauss();
+                break;
+        }
     }
 
     /// <summary>
     /// 가우스의 가호 : 세레스 공전, 이 다음 공격 데미지를 두배로.
     /// </summary>
-    public void ProtectionOfGauss()
+    public void GraceOfGauss()
     {
+        if (gaussOn) return;
+        gaussOn = true;
         Setup();
         float radius = 200f;
         float angle = 0f;
@@ -33,7 +52,7 @@ public class ProtectionManager
         // 공격 데미지 2배
         Managers.Game.Damage *= 2;
 
-        GameObject Ceres = Managers.Resource.Instantiate("protection/Ceres", player.transform);
+        GameObject Ceres = Managers.Resource.Instantiate("Grace/Ceres", player.transform);
         Ceres.transform.position = new Vector3(playerPos.x - radius, playerPos.y, 0);
 
         // 세레스 돌리기
@@ -61,6 +80,7 @@ public class ProtectionManager
                 Debug.Log("witch hp : " + witch.Hp);
                 Managers.Game.Damage = Managers.Game.Damage / 2;
                 Object.Destroy(Ceres.gameObject);
+                gaussOn = false;
                 yield break;
             }
 
