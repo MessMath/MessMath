c640eacc783577903c35cbbdf3e759d0dba58c77
BJH <a01091363615@gmail.com>
2023-08-23 12:40:23 +0900

    [ADD] 수학자 선택 팝업 창 한영버전 추가 (-정훈)

--- Assets/1_Scripts/I18n/I18nDefine.cs
+++ Assets/1_Scripts/I18n/I18nDefine.cs
@@ -4,4 +4,12 @@ public static class I18nDefine
     public const string STORE_GRACE = "STORE_GRACE";
     public const string STORE_COLLECTIONS = "STORE_COLLECTIONS";
     public const string STORE_CLOTHES = "STORE_CLOTHES";
+
+    public const string CHOOSE_BOSS_TEXT = "CHOOSE_BOSS_TEXT";
+    public const string MathMtcName1 = "MathMtcName1";
+    public const string QuestionNum1 = "QuestionNum1";
+    public const string MathMtcName2 = "MathMtcName2";
+    public const string QuestionNum2 = "QuestionNum2";
+    public const string MathMtcName3 = "MathMtcName3";
+    public const string QuestionNum3 = "QuestionNum3";
 }
