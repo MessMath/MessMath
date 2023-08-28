bde1d2d605ddf46380255d5ad40c3a5564bbec93
BJH <a01091363615@gmail.com>
2023-08-28 17:29:22 +0900

    [UPDATE] Epilogue 한영버전 적용 중 (-정훈)

--- Assets/1_Scripts/UI/Scene/UI_Epilogue.cs
+++ Assets/1_Scripts/UI/Scene/UI_Epilogue.cs
@@ -1,3 +1,4 @@
+using MessMathI18n;
 using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,6 +14,27 @@ public class UI_Epilogue : UI_Scene
         Image3,
         FadeIn, 
         Credits,
+    }
+
+    enum Texts
+    {
+        °³¹ß,
+        ¿ÀÇÕÁöÁ¹_´ëÀå_¼­ÇöÀç1,
+        ¿ÀÇÕÁöÁ¹_¼­Àå_À±Áö¿¬1,
+        ¿ÀÇÕÁöÁ¹_½ÉÀå_Á¤¼öÁø1,
+        ¿ÀÇÕÁöÁ¹_²¿Àå_¹èÁ¤ÈÆ1,
+        ¾ÆÆ®,
+        ¿ÀÇÕÁöÁ¹_»õÀå_¼­¹Î¿µ1,
+        ±âÈ¹,
+        ¿ÀÇÕÁöÁ¹_´ëÀå_¼­ÇöÀç2,
+        ¿ÀÇÕÁöÁ¹_¼­Àå_À±Áö¿¬2,
+        ¿ÀÇÕÁöÁ¹_½ÉÀå_Á¤¼öÁø2,
+        ¿ÀÇÕÁöÁ¹_²¿Àå_¹èÁ¤ÈÆ2,
+        ¿ÀÇÕÁöÁ¹_»õÀå_¼­¹Î¿µ2,
+        ¾Æ¹«Æ°_Å©·¹µ÷,
+        Team_¿ÀÇÕÁöÁ¹_Presents,
+        AndYou,
+        BackToLobby,
     }
 
     enum Buttons
@@ -34,6 +56,11 @@ public class UI_Epilogue : UI_Scene
         GetImage((int)Images.Image2).gameObject.BindEvent(() => GetImage((int)Images.Image3).gameObject.SetActive(true));
         GetImage((int)Images.Image3).gameObject.BindEvent(ShowCredits);
         GetImage((int)Images.Credits).gameObject.SetActive(false);
+
+        GetText((int)Texts.°³¹ß).text = I18n.Get(I18nDefine.LOBBY_SETTING);
+
+
+
 
         GetButton((int)Buttons.BackToLobbyBtn).gameObject.BindEvent(()=>{ Managers.Sound.Play("ClickBtnEff");  Managers.Scene.ChangeScene(Define.Scene.LobbyScene); });
 
