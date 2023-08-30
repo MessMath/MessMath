cb5a93f5a3ef1fd18a2d9ffe7e8a7dba71101947
truemanburbank <juxtap.se@gmail.com>
2023-08-31 04:39:47 +0900

    [FEAT] 로비 튜토리얼 추가 & 닉네임 디비 설정(-수진)

--- Assets/1_Scripts/Manager/DatabaseManager.cs
+++ Assets/1_Scripts/Manager/DatabaseManager.cs
@@ -19,12 +19,12 @@ public class DatabaseManager : MonoBehaviour
     {
         Debug.Log("CreateNewUser");
         //WriteNewUser(Managers.GoogleSignIn.GetUID(), 0, 0, false, false, false, nickname);
-        WriteNewUser(nickname, 0, 0, false, false, false);
+        WriteNewUser(nickname, 0, 0, false, false, false, nickname);
     }
 
-    private void WriteNewUser(string userId, int coin, int score, bool isCompletedStory, bool isCompletedTutorial, bool isCompletedDiagnosis/*, string nickname*/)
+    private void WriteNewUser(string userId, int coin, int score, bool isCompletedStory, bool isCompletedTutorial, bool isCompletedDiagnosis, string nickname)
     {
-        Managers.UserMng.InitUser(userId, coin, score, null, isCompletedStory, isCompletedTutorial, isCompletedDiagnosis/*, nickname*/, null, null, "안녕하세요!");
+        Managers.UserMng.InitUser(userId, coin, score, null, isCompletedStory, isCompletedTutorial, isCompletedDiagnosis, nickname, null, null, "안녕하세요!");
 
         string json = JsonUtility.ToJson(Managers.UserMng.user);
         Debug.Log("WriteNewUser");
---
+++ Assets/1_Scripts/UI/Popup/UI_LobbyTutorial.cs
@@ -0,0 +1,111 @@
+using MessMathI18n;
+using StoryData;
+using System.Collections;
+using System.Collections.Generic;
+using TutorialDatas;
+using UnityEngine;
+
+public class UI_LobbyTutorial : UI_Popup
+{
+    JsonReader jsonReader;
+    List<TalkData> storyTalkData = new List<TalkData>();
+    int maxCount;
+    int count = 0;
+
+    enum GameObjects
+    {
+        Panel,
+    }
+
+    enum Images
+    {
+        BackGroundImage,
+        PlayerImage,
+        CharacterImage,
+        CharacterBG,
+    }
+    enum Buttons
+    {
+        nxtButton,
+    }
+
+    enum Texts
+    {
+        CharacterNameTMP,
+        DialogueTMP,
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        BindObject(typeof(GameObjects));
+        BindImage(typeof(Images));
+        BindButton(typeof(Buttons));
+        BindText(typeof(Texts));
+
+        jsonReader = new JsonReader();
+
+        if (LocalizationManager.Get().GetSelectedLanguage() == Language.KOREAN)
+        {
+            storyTalkData = jsonReader.ReadStoryJson(Application.persistentDataPath + "/" + 12 + "_LobbyTutorial_KOR.json").talkDataList;
+        }
+        else
+        {
+            storyTalkData = jsonReader.ReadStoryJson(Application.persistentDataPath + "/" + 13 + "_LobbyTutorial_EN.json").talkDataList;
+        }
+
+        maxCount = storyTalkData.Count;
+
+        GetText((int)Texts.CharacterNameTMP).text = "";
+        GetText((int)Texts.DialogueTMP).text = "";
+        GetButton((int)Buttons.nxtButton).gameObject.BindEvent(OnClickNxtBtn);
+
+        return true;
+    }
+
+    public void OnClickNxtBtn()
+    {
+        if (!Managers.TextEffect.isTypingEnd)
+        {
+            Managers.TextEffect.SetFastSpeed();
+            return;
+        }
+        if (++count >= maxCount)
+        {
+            PlayerPrefs.SetInt("DoTutorial", 2);
+            Managers.UI.ClosePopupUI(this);
+            return;
+        }
+
+        PlayerPrefs.SetInt("WatchedStory", count);
+        Managers.SceneEffect.ChangeCharacterBG(GetImage((int)Images.CharacterBG), storyTalkData[count].characterName);
+        Managers.SceneEffect.ChangeCharacter(GetImage((int)Images.PlayerImage), GetImage((int)Images.CharacterImage), storyTalkData[count].characterName, storyTalkData[count].expression);
+
+
+        if (storyTalkData[count].txtEffect == "MAX")
+        {
+            GetText((int)Texts.DialogueTMP).fontSize = 100;
+        }
+        else
+        {
+            GetText((int)Texts.DialogueTMP).fontSize = 80;
+        }
+
+        GetText((int)Texts.CharacterNameTMP).text = storyTalkData[count].characterName;
+
+        Managers.TextEffect.SetNormalSpeed();
+        Managers.TextEffect.Typing(storyTalkData[count].dialogue, GetText((int)Texts.DialogueTMP));
+
+        // Sound
+        Managers.Sound.Play("ClickBtnEff");
+        //talkDatas[i].soundEffect
+        //talkDatas[i].soundEffectDuration
+    }
+}
