2098c062a5c787affddff331a39eba7c809c634a
truemanburbank <juxtap.se@gmail.com>
2023-08-30 18:35:45 +0900

    [CHORE] 튜토리얼 영어 버전 파일 읽어오기 오류 수정
    Assets\1_Scripts\UI\Popup\UI_TutorialPopup.cs - 파일 경로(이름) 수정

--- Assets/1_Scripts/Manager/Managers.cs
+++ Assets/1_Scripts/Manager/Managers.cs
@@ -75,7 +75,7 @@ public class Managers : MonoBehaviour
     {
         if (s_instance == null)
         {
-            //PlayerPrefs.DeleteAll();
+            PlayerPrefs.DeleteAll();
             //PlayerPrefs.SetInt("WatchedStory", -2);
             PlayerPrefs.SetInt("Coin", 10000);
             GameObject go = GameObject.Find("@Managers");
